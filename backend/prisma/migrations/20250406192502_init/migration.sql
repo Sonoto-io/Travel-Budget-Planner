e56b2f273376239cbec33ec8a1c2691573bb9c65
Sonoto <contact@sonoto.io>
2025-04-06 21:56:06 +0200

    setting up bun :tada:

--- backend/prisma/migrations/20250406192502_init/migration.sql
+++ backend/prisma/migrations/20250406192502_init/migration.sql
@@ -16,5 +16,35 @@ CREATE TABLE "Country" (
     CONSTRAINT "Country_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Expense" (
+    "id" SERIAL NOT NULL,
+    "countryId" INTEGER NOT NULL,
+    "categoryId" INTEGER,
+    "name" TEXT NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL DEFAULT 0.00,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Country_name_key" ON "Country"("name");
