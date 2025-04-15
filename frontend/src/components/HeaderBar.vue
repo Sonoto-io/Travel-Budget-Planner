dc3b588c8c613c1ec94ff28c89a8afb422a83117
Sonoto <contact@sonoto.io>
2025-04-15 21:32:21 +0200

    start making frontend layout and config

---
+++ frontend/src/components/HeaderBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Image } from "primevue";
+//TODO: add tailwindcss
+</script>
+
+<template>
+  <div class="w-full flex flex-row">
+    <button class="visible md:invisible"><icon class="pi pi-check" /></button>
+    <p>Travel Planner</p>
+    <Image />
+    <div>v0.0.1</div>
+  </div>
+</template>
---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,6 @@
+<template>
+  <header><slot name="header" /></header>
+  <navbar><slot name="menu" /></navbar>
+  <main><slot /></main>
+  <aside><slot name="data" /></aside>
+</template>
