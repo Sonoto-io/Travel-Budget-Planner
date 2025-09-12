037fb758d4624268711fcd6ace40e3043bd257cb
Sonata <account@sonoto.io>
2025-09-12 16:06:52 +0800

    wip

---
+++ frontend/tests/unit/views/ManagementView.test.ts
@@ -0,0 +1,13 @@
+import ManagementView from "@/views/ManagementView.vue";
+import { describe, test, expect, vi, beforeEach } from "vitest";
+import { mount } from "@vue/test-utils";
+
+describe.concurrent("ManagementView", () => {
+
+  test("Shows correct components", () => {
+    const wrapper = mount(ManagementView);
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.text()).toContain("Management");
+    expect(wrapper.findComponent({ name: "ManagementMain" }).exists()).toBe(true);
+  });
+});
