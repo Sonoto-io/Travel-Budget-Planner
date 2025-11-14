f7dc2b24f451017355d414428ce556de895a10e0
Sonata <account@sonoto.io>
2025-11-14 10:32:00 +0900

    add expenses tests

---
+++ backend/tests/factories/expenseFactories.ts
@@ -0,0 +1,68 @@
+import type { Prisma, Expense } from "@prisma/client";
+import { randomUUIDv7 } from "bun";
+
+// interface IRepartition {
+//     name: string;
+//     totalExpenses: number;
+//     countExpenses: number;
+//     subcategories?: Array<IRepartition>
+// }
+
+// export function summaryFactory(overrides: Partial<ISummary> = {}): ISummary {
+//   return {
+//     totalExpenses: 100.5,
+//     countExpenses: 1,
+//     expectedCountDays: 
+//     countDays: number;
+//     dailyExpenses: number;
+//     expectedDailyExpenses: number;
+//     totalExpectedExpense?: number;
+//     repartition: Array<IRepartition>
+//   }
+// }
+
+// interface ISummary {
+//   totalExpenses: number;
+//   countExpenses: number;
+//   expectedCountDays: number;
+//   countDays: number;
+//   dailyExpenses: number;
+//   expectedDailyExpenses: number;
+//   totalExpectedExpense?: number;
+//   repartition: Array<IRepartition>;
+// }
+
+export function expensesFactory(overrides: Partial<Prisma.ExpenseCreateInput> = {}): Expense {
+  return {
+    id: randomUUIDv7(),
+    country: {
+      id: randomUUIDv7(),
+      name: "testCountry",
+    },
+    user: {
+      id: randomUUIDv7(),
+      label: "testUsername",
+    },
+    category: {
+      id: randomUUIDv7(),
+      label: "testCategory",
+    },
+    subcategory: {
+      id: randomUUIDv7(),
+      label: "testSubacategory",
+    },
+    currency: {
+      id: randomUUIDv7(),
+      locale: "fr-FR",
+      name: "testCurrencyName",
+      label: "testCurrencyLabel",
+      conversion: 1.00,
+    },
+    note: "testNote",
+    price: 100.50,
+    date: new Date(),
+    location: "testLocation",
+    exception: false,
+    ...overrides,
+  };
+}
---
+++ backend/tests/routes/expensesRoutes.spec.ts
@@ -0,0 +1,63 @@
+import { describe, expect, it, mock } from "bun:test";
+import { api } from "../setup";
+import type { Expense, Prisma } from "@prisma/client";
+import { expensesFactory } from "../factories/expenseFactories";
+
+mock.module("@/repositories/expensesRepository.ts", () => ({
+  expenseRepository: {
+    async getAll(options?: Prisma.ExpenseFindManyArgs): Promise<Array<Expense>> {
+      return [expensesFactory()]
+    },
+    async getForCountry(countryId: string): Promise<Array<Expense>> {
+      return [expensesFactory({country: { id: countryId }})]
+    },
+    async create(expense: Prisma.ExpenseCreateInput): Promise<Expense> {
+      return expensesFactory({...expense})
+    },
+    async update(expenseId: string, expense: Prisma.ExpenseUpdateInput) {
+      return expensesFactory({ id: expenseId, ...expense })
+    },
+    async delete(expenseId: string) {
+      return expensesFactory({ id: expenseId })
+    }
+  }
+}))
+
+
+
+
+describe("Expense", () => {
+  it("creates an expense successfully", async () => {
+    const response = await api.expenses.post(expensesFactory({ price: 150.75, note: "New Expense" }));
+    expect(response.status).toBe(200);
+    expect(response.data.message).toBe("Expense created");
+    expect(response.data.data.expense.price).toBe(150.75);
+    expect(response.data.data.expense.note).toBe("New Expense")
+  });
+
+  it("retrieves all expenses successfully", async () => {
+    const response = await api.expenses.get();
+    expect(response.status).toBe(200);
+    expect(response.data.expenses).toBeArray();
+  });
+
+  it("updates an expense successfully", async () => {
+    const response = await api.expenses["123"].post({
+      price: 1234,
+      date: new Date()
+    });
+    console.log(response.error)
+    expect(response.status).toBe(200);
+    expect(response.data.message).toBe("Expense updated");
+  });
+  
+  it("deletes an expense successfully", async () => {
+    const response = await api.expenses["123"].delete();
+    expect(response.status).toBe(200);
+    expect(response.data.message).toBe("Expense deleted");
+  });
+
+  // Summaries
+
+  
+});
