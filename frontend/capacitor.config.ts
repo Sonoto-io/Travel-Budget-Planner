ecd7509cb2b13aec0b6de4cd5ee5288455bac427
Sonata <account@sonoto.io>
2026-01-01 19:52:11 +0700

    integrate capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.sonoto.travelbudgetplanner',
+  appName: 'Travel Budget Planner',
+  webDir: 'dist'
+};
+
+export default config;
--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,5 +1,7 @@
 <template>
-  <Button type="button" severity="secondary" label="Login with SSO" @click="login"/>
+  <div class="flex justify-center items-center mt-6 w-screen h-screen">
+    <Button type="button" class="w-[30vw] h-[10vh]" severity="secondary" label="Login with SSO" @click="login"/>
+  </div>
 </template>
 
 <script setup lang="ts">
