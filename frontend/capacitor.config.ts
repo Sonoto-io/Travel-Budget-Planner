c672a218015a45e5ceba0d527ff60755a01bc21e
Sonata <account@sonoto.io>
2026-01-06 19:52:21 +0700

    wip

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.sonoto.travelbudgetplanner',
   appName: 'Travel Budget Planner',
-  webDir: 'dist'
+  webDir: 'dist',
+  
+  server: {
+    url: 'https://travelbudget.ensbf-holdings.com',
+    cleartext: false,
+  },
 };
+
 
 export default config;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,12 +9,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: ["travelbudget.ensibf-holdings.com/api"]
   },
   test: {
     globals: true,
     environment: "jsdom",
     env: {
-      VITE_API_URL: "http://app.localhost",
+      VITE_API_URL: "http://travelbudget.ensibf-holdings.com/api",
     },
     setupFiles: ['./vitest.setup.ts'],
     include: ["tests/**/*.{test,spec}.{ts,js}"],
