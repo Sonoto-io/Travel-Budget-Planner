24bebd671340d4275e0f35a6872ca68603462757
Sonata <account@sonoto.io>
2026-01-15 21:23:07 +0700

    remove occurence of url

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'fr.sonoto.travelbudget',
   appName: 'Travel Budget Planner',
   webDir: 'dist',
-    server: {
-    iosScheme: 'https',
-    hostname: 'travelbudget.ensibf-holdings.fr',
-  },
 };
 
 
