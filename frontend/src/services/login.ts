8e2c56b2e5a7c495e13bbbf8313d4aabd1937a71
Sonata <account@sonoto.io>
2026-01-15 21:18:41 +0700

    change api client to work for native

--- frontend/src/services/login.ts
+++ frontend/src/services/login.ts
@@ -1,17 +1,15 @@
 import { useAuthStore } from "@/stores/authStore";
 import { pinia } from "@/pinia";
 import api from "@/api/apiClient";
-import { CapacitorHttp } from '@capacitor/core';
 
 const isAuthenticated = async () => {
   // verify session cookie is valid from backend
   try {
-    const res = await CapacitorHttp.post({
-    url: 'https://travelbudget.ensibf-holdings.fr/api/auth/verify-session',
-    webFetchExtra: {
-      credentials: 'include',
-    },
-  });
+    console.log("try to authenticate")
+    const res = api.post("/auth/verify-session", {
+      method: "POST",
+      credentials: "include"
+    });
     console.log("authenticate response : ", JSON.stringify(await res))
     const valid = (await res).data.valid;
     useAuthStore(pinia).setAuthenticated(valid);
@@ -24,35 +22,22 @@ const isAuthenticated = async () => {
 };
 
 export const getTokenFromCode = async (code: string) => {
-  // const res = api.post(
-  //   "/auth/finalize",
-  //   { code },
-  //   {
-  //     withCredentials: true,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   }
-  // ).then(response => {
-  //   console.log("response: ", JSON.stringify(response))
-  //   return response;
-  // }).catch(error => {
-  //   console.error("Error finalizing authentication:", JSON.stringify(error));
-  //   throw "Error finalizing authentication";
-  // });
-
-  const res = await CapacitorHttp.post({
-    url: 'https://travelbudget.ensibf-holdings.fr/api/auth/finalize',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: { code },
-    webFetchExtra: {
-      credentials: 'include',
-    },
+  const res = api.post(
+    "/auth/finalize",
+    { code },
+    {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  ).then(response => {
+    console.log("response: ", JSON.stringify(response))
+    return response;
+  }).catch(error => {
+    console.error("Error finalizing authentication:", JSON.stringify(error));
+    throw "Error finalizing authentication";
   });
-
-  console.log("res : ", JSON.stringify(res))
 
   return res
 }
